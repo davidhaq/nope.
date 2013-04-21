b468dee94df074acca06ee6f2c59c531b027800a
Justin Harrison <justin.harrison1337@gmail.com>
2013-04-21 13:16:19 -0500

    Adding several constants for data storage

--- src/com/jphsoftware/nope/Constants.java
+++ src/com/jphsoftware/nope/Constants.java
@@ -2,6 +2,7 @@ package com.jphsoftware.nope;
 
 public class Constants {
 	
+	public static final String CALLBLOCK_DATA = "call_block_data";
 	public static final String BLOCKED_NUMBERS = "blocked_nums";
 	public static final String BLOCKED_NUMBERS_LAST_CALL = "last_called";
 
