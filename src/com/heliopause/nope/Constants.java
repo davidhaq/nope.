4879eb982b25f1250e68d5e9a36bc7db879ddd83
Justin Harrison <justin.harrison1337@gmail.com>
2013-09-18 00:24:31 -0500

    Changed contstants file to match keys of preferences fragment.

--- src/com/heliopause/nope/Constants.java
+++ src/com/heliopause/nope/Constants.java
@@ -3,8 +3,8 @@ package com.heliopause.nope;
 public class Constants {
 	
 	public static final String SETTINGS_PREFS = "settingssharedpreferenceskey";
-	public static final String MSG_BLOCK_SERVICE_STATUS = "msgblockservicekey";
-	public static final String CALL_BLOCK_SERVICE_STATUS = "callblockservicekey";
-	public static final String SPAM_BLOCK_SERVICE_STATUS = "spamblockservicekey";
+	public static final String MSG_BLOCK_SERVICE_STATUS = "pref_key_text_block_service_state";
+	public static final String CALL_BLOCK_SERVICE_STATUS = "pref_key_call_block_service_state";
+	public static final String SPAM_BLOCK_SERVICE_STATUS = "pref_key_spam_block_service_state";
 
 }
